1e91b3bf09467dfdce8bef9cb6bb20fd3286794c
David Serna Restrepo <david.sernar@uqvirtual.edu.co>
2023-06-21 08:20:27 -0500

    Initial Commit

---
+++ src/main/java/org/steamclone/SteamCloneApplication.java
@@ -0,0 +1,14 @@
+package org.steamclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamCloneApplication.class, args);
+    }
+
+
+}
